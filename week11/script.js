0d3e000ace1b86b917fa081d33439fb9503ef83e
DerekDincer <32680622+DerekDincer@users.noreply.github.com>
2020-11-11 19:32:13 +0300

    Update script.js

--- week11/script.js
+++ week11/script.js
@@ -10,22 +10,16 @@ moreButt.addEventListener('click', more);
 
 
 
-
 let page = 0;
 
 function more(){
   page += 1
   if (page == 1){
-    para.textContent = "Firstly, it made me think about the difference between a technological product and the service it provides. Just as the medium provides the message, the product provides a service. Are the product and service inseperable and/or one and the same?";
-  }
-
-  if (page == 2){
-    para.textContent = "Secondly, it reminded be of the function and form properties of a piece of art. In particular, substance and style in cinema. I remember clearly that a reviewer had argued 'The Man from U.N.C.L.E.' was a good piece of cinema while everyone else thought it was flashy but lacking in the story department. This reviewer claimed that U.N.C.L.E.'s style (medium) was its substance (message) and that the rest didn't really matter.";
+    para.textContent = "Firstly, it made me think about the difference between a technological product and the service it provides. Just as the medium provides the message, the product provides a service. Are the product and service inseperable and/or one and the same? Secondly, it reminded be of the function and form properties of a piece of art. In particular, substance and style in cinema. I remember clearly that a reviewer had argued 'The Man from U.N.C.L.E.' was a good piece of cinema while everyone else thought it was flashy but lacking in the story department. This reviewer claimed that U.N.C.L.E.'s style (medium) was its substance (message) and that the rest didn't really matter.";
     moreButt.style.visibility = "hidden";
   }
 }
 
 function home{
-
   homeButt.href = "https://derekdincer.github.io/"
 }
